fef3ca6aef328517b6707f51e8ea3ee555221c25
Olivier Girardot <olive_girardot@hotmail.com>
2020-09-01 14:42:12 +0200

    added some comments to explain what is happening

--- js/stretch.directive.js
+++ js/stretch.directive.js
@@ -2,9 +2,10 @@ function stretch() {
     return {
         restrict: 'A',
         link: function ($scope, $element, $attrs) {
-            var element = $element[0];
-            element.addEventListener('focus', function() {
-                this.style.width = "500px";
+            var element = $element[0]; // jquery syntax equivalent to const documrny.querySelector('.something')
+            console.log($attrs);
+            element.addEventListener('focus', function() { // focus is like ng-focus, same for blur bellow
+                this.style.width = "500px";  // style accesses the css
             });
             element.addEventListener('blur', function() {
                 this.style.width = "120px";
